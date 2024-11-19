0e8817d5556fa3578f34a5b10eb05a34d89a36a0
Heilan4 <olan.heinen@student.unamur.be>
2024-11-19 16:37:09 +0100

    Book opened by default
    Disabled the opening/closing animation as the Joint glitch couldn't be fixed. Assets still present for eventual fix.

--- Assets/ToggleBook.cs
+++ Assets/ToggleBook.cs
@@ -2,9 +2,16 @@ using UnityEngine;
 
 public class ToggleBook : MonoBehaviour
 {
+    public GameObject book;
+    public HingeJoint hinge;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     public void Toggle()
     {
+        if (hinge != null) {
+            var hinge = book.GetComponent<HingeJoint>();
+            hinge.useMotor = false;
+        }
+
         gameObject.SetActive(!gameObject.activeSelf);
     }
 }
--- Assets/openBook.cs
+++ Assets/openBook.cs
@@ -4,6 +4,8 @@ public class openBook : MonoBehaviour
 {
     public GameObject book;
     public HingeJoint hinge;
+    public JointMotor motor;
+    public bool opened = false;
 
     void Start()
     {
@@ -13,6 +15,23 @@ public class openBook : MonoBehaviour
 
     public void Opening()
     {
-        hinge.useMotor = true;
+        if (!opened) {
+            motor = hinge.motor;
+            hinge.useMotor = true;
+            opened = true;
+        }
+    }
+
+    public void Closing()
+    {
+        if (opened) {
+            JointLimits limits = hinge.limits;
+            limits.min = -170;
+            limits.max = 0;
+            hinge.limits = limits;
+            motor.targetVelocity = -150;
+            hinge.useMotor = true;
+            opened = false;
+        }
     }
 }
