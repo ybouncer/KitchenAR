1efb10671a8d85dc5788e6b126f7f785c73812c9
Heilan4 <olan.heinen@student.unamur.be>
2024-12-02 19:32:31 +0100

    Improved interactions

--- Assets/ToggleBook.cs
+++ Assets/ToggleBook.cs
@@ -4,6 +4,8 @@ public class ToggleBook : MonoBehaviour
 {
     public GameObject book;
     public GameObject parentbook;
+    public GameObject CanvasLeft;
+    public GameObject CanvasRight;
     public HingeJoint hinge;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     public void Toggle()
@@ -20,11 +22,34 @@ public class ToggleBook : MonoBehaviour
             hinge.axis = -hinge.axis;
             hinge.useMotor = true;
         }*/
+        
         foreach (Transform child in parentbook.transform) {
             Renderer childrend = child.GetComponent<Renderer>();
             childrend.enabled = !childrend.enabled;
+            //DisableRenderers(child.gameObject);
         }
+        CanvasLeft.SetActive(!CanvasLeft.activeSelf);
+        CanvasRight.SetActive(!CanvasRight.activeSelf);
+        
+
         //hinge.enabled = !hinge.enabled;
         //gameObject.SetActive(!gameObject.activeSelf);
     }
+
+void DisableRenderers(GameObject parentObject)
+    {
+        // Disable the Renderer component for the current GameObject
+        Renderer renderer = parentObject.GetComponent<Renderer>();
+        if (renderer != null)
+        {
+            renderer.enabled = false;
+        }
+
+        // Recursively call the function for each child of the current GameObject
+        foreach (Transform child in parentObject.transform)
+        {
+            DisableRenderers(child.gameObject); // Recursion: process each child GameObject
+        }
+    }
+
 }
